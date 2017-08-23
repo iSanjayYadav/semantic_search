d37eea24b1852801757b63c7fc742e4505d3a7db
sophiazwilson <szwwilson@gmail.com>
2017-08-23 16:06:15 -0700

    with SQL

--- docker/postgres/initdb.sql
+++ docker/postgres/initdb.sql
@@ -1,3 +1,17 @@
-CREATE TABLE wiki_stuff (
-    id INTEGER
+CREATE TABLE pages(
+    page_id INTEGER PRIMARY KEY,
+    page_title TEXT,
+    page_text TEXT
+);
+
+CREATE TABLE category (
+	category_id INTEGER PRIMARY KEY,
+	category_title TEXT
+);
+
+
+CREATE TABLE page_category(
+	page_category_id SERIAL PRIMARY KEY,
+	category_id INTEGER,
+	page_id INTEGER  
 );
